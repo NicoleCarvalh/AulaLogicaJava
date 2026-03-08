0bf002f96b9cbedac013e97e8b4d40b3879a62ea
Nicole Carvalho <91771078+NicoleCarvalh@users.noreply.github.com>
2026-03-08 11:17:57 -0300

    Update Aula02_Ex6.java

--- src/atividadesAula02/Aula02_Ex6.java
+++ src/atividadesAula02/Aula02_Ex6.java
@@ -5,16 +5,61 @@ import java.util.Scanner;
 public class Aula02_Ex6 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         int contagemVendedor = 1;
+        int contagemDia = 1;
+        String nomeVendedor = "";
+        double valorVendidoVendedor;
+        double totalGeralVendasEquipe = 0;
 
         do {
-            System.out.println("Insira o valor vendido pelo vendedor " + contagemVendedor);
+            for (int i = 1; contagemVendedor <= 5; i++){
+                switch (contagemVendedor){
+                    case 1:
+                        nomeVendedor = "Julia";
+                        break;
+                    case 2:
+                        nomeVendedor = "Cláudio";
+                        break;
+                    case 3:
+                        nomeVendedor = "Fernanda";
+                        break;
+                    case 4:
+                        nomeVendedor = "Márcio";
+                        break;
+                    case 5:
+                        nomeVendedor = "René";
+                        break;
+                    default:
+                        nomeVendedor = "Vendedor";
+                        break;
+                }
 
+                System.out.println(" ----------VENDAS DIA " + contagemDia + "----------");
+                System.out.println("Insira o valor vendido pelo vendedor(a) " + contagemVendedor + " - " + nomeVendedor);
+                valorVendidoVendedor = sc.nextDouble();
 
-            contagemVendedor++;
-        } while(contagemVendedor <= 5);
+                totalGeralVendasEquipe += valorVendidoVendedor;
 
+                if (totalGeralVendasEquipe >= 1000){
+                    System.out.println("A meta de R$1000 foi atingida por " + nomeVendedor);
+                    System.out.println("Com a compra no valor de " + valorVendidoVendedor);
+                    System.out.println("Total de vendas realizadas pela equipe é de " + valorVendidoVendedor);
 
+                    break;
+                }
+
+                contagemVendedor++;
+            }
+
+            contagemDia++;
+
+        } while(contagemDia <= 6);
+
+        if (totalGeralVendasEquipe < 1000){
+            System.out.println("Meta de R$1000 não foi atingida.");
+            System.out.println(" Total acumulado: " + totalGeralVendasEquipe);
+        }
 
     }
 }
